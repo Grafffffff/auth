f3ca0463a9ecd312d87a4fd570839cf76b33497c
Anton <antonok94@gmail.com>
2019-07-25 03:41:30 +0400

    Update core.js

--- core.js
+++ core.js
@@ -5,114 +5,106 @@ const hideNode = (node, bool) => bool
   ? node.classList.remove('hidden')
   : node.classList.add('hidden');
 
+class AuthorizationForm {
+  constructor(form, setState) {
+    this.form = form;
+    this.logInBtn = form.querySelector('.authorization-form__btn');
+    this.email = form.querySelector('input[type=email]');
+    this.password = form.querySelector('input[type=password]');
+    this.errorField = form.querySelector('.authorization-form__error');
+
+    this.callback = setState;
+    this.logInBtn.onclick = this.login;
+  }
+
+  initForm = (display) => {
+    hideNode(this.form, display);
+
+    this.email.value = '';
+    this.email.classList.remove('input-error');
+
+    this.password.classList.remove('input-error');
+    this.password.value = '';
+
+    this.errorField.classList.add('hidden');
+  };
+
+  validateForm =() => {
+    this.email.classList.remove('input-error');
+    this.password.classList.remove('input-error');
+    this.errorField.classList.add('hidden');
+
+    if (!this.email.value || !emailRegExp.test(this.email.value)) {
+      this.email.classList.add('input-error');
+      return "E-Mail is incorrect";
+    }
+    if (!this.password.value) {
+      this.password.classList.add('input-error');
+      return 'Password has required';
+    }
+  };
+
+  displayError = (error) => {
+    this.errorField.innerText = error;
+    this.errorField.classList.remove('hidden');
+  };
+
+  login = () => {
+    const error = this.validateForm();
+
+    if (error) return this.displayError(error);
+
+    this.sendRequest()
+      .then(data => {
+        if(data.error) throw Error(data.error);
+        return this.callback({authorized: true, ...data})})
+      .catch(error => this.displayError(error))
+  };
+
+  sendRequest = () => {
+    const url = 'https://us-central1-mercdev-academy.cloudfunctions.net/login';
+    const user = JSON.stringify({
+      email: this.email.value,
+      password: this.password.value
+    });
+
+    return fetch(url, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: user
+    })
+      .then(res => res.json())
+  }
+}
+
+class Profile {
+  constructor(profile, setInitialState) {
+    this.profile = profile;
+    this.logOutBtn = profile.querySelector('.authorization-form__btn');
+    this.photo = profile.querySelector('.profile__photo');
+    this.name = profile.querySelector('.profile__name');
+    this.logOutBtn.onclick = this.logout;
+
+    this.callback = setInitialState;
+    this.currentState = {};
+  }
+
+  initForm = (display, state) => {
+    hideNode(this.profile, display);
+    this.currentState = state;
+    if (this.currentState.authorized) this.createProfile()
+  };
+
+  logout = () => this.callback();
+
+  createProfile = () => {
+    this.photo.src = this.currentState.photoUrl;
+    this.name.innerText = this.currentState.name;
+  }
+}
+
 window.onload = function(){
   let state = {};
-
-  const form = document.querySelector('.authorization-form');
-  const profile = document.querySelector('.profile');
-
-  class AuthorizationForm {
-    constructor(form, display) {
-      this.form = form;
-      this.logInBtn = form.querySelector('.authorization-form__btn');
-      this.email = form.querySelector('input[type=email]');
-      this.password = form.querySelector('input[type=password]');
-      this.errorField = form.querySelector('.authorization-form__error');
-
-      this.initForm(display);
-    }
-
-    initForm = (display) => {
-      hideNode(this.form, display);
-
-      this.email.value = '';
-      this.email.classList.remove('input-error');
-
-      this.password.classList.remove('input-error');
-      this.password.value = '';
-
-      this.errorField.classList.add('hidden');
-
-      this.logInBtn.onclick = this.login;
-    };
-
-    validateForm =() => {
-      this.email.classList.remove('input-error');
-      this.password.classList.remove('input-error');
-      this.errorField.classList.add('hidden');
-
-      if (!this.email.value || !emailRegExp.test(this.email.value)) {
-        this.email.classList.add('input-error');
-        return "E-Mail is incorrect";
-      }
-      if (!this.password.value) {
-        this.password.classList.add('input-error');
-        return 'Password has required';
-      }
-    };
-
-    displayError = (error) => {
-      this.errorField.innerText = error;
-      this.errorField.classList.remove('hidden');
-    };
-
-    login = () => {
-      const error = this.validateForm();
-
-      if (error) return this.displayError(error);
-
-      this.sendRequest()
-        .then(data => {console.log(data);
-          if(data.error) throw Error(data.error);
-          return state = {authorized: true, ...data}})
-        .then(() => render())
-        .catch(error => this.displayError(error))
-    };
-
-    sendRequest = () => {
-      const url = 'https://us-central1-mercdev-academy.cloudfunctions.net/login';
-      const user = JSON.stringify({
-        email: this.email.value,
-        password: this.password.value
-      });
-
-      return fetch(url, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: user
-      })
-        .then(res => res.json())
-    }
-  }
-
-  class Profile {
-    constructor(profile, display) {
-      this.profile = profile;
-      this.logOutBtn = profile.querySelector('.authorization-form__btn');
-      this.photo = profile.querySelector('.profile__photo');
-      this.name = profile.querySelector('.profile__name');
-
-      this.initForm(display);
-    }
-
-    initForm = (display) => {
-      hideNode(this.profile, display);
-      this.logOutBtn.onclick = this.logout;
-      if (state.authorized) this.createProfile()
-    };
-
-    logout = () => setInitialState();
-
-    createProfile = () => {
-      this.photo.src = state.photoUrl;
-      this.name.innerText = state.name;
-    }
-  }
-
-  function render() {
-    new AuthorizationForm(form, !state.authorized);
-    new Profile(profile, state.authorized)
-  }
 
   const setInitialState = () => {
     state = {
@@ -124,5 +116,20 @@ window.onload = function(){
     render()
   };
 
-  setInitialState()
+  function setState(newState) {
+    state = {
+      ...newState
+    };
+    render()
+  }
+
+  const form = new AuthorizationForm(document.querySelector('.authorization-form'), setState);
+  const profile = new Profile(document.querySelector('.profile'), setInitialState);
+
+  function render() {
+    form.initForm(!state.authorized);
+    profile.initForm(state.authorized, state);
+  }
+
+  setInitialState();
 };
